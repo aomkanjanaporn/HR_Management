7c42cfee99e462daa3032678b9c02849b2ca1621
DESKTOP-FUI4BF1\Aommy kjp'n <aommy05349@gmail.com>
2019-05-29 16:39:39 +0700

    Revert "upload"
    This reverts commit a5e7e3a114e764ed98744e7d09822fb94ea7f01a.

--- resources/views/layouts/partials/content.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="main-panel">
-    <div class="content-wrapper">
-    </div>
-    @include('Layouts.partials.footer')
-        
-</div>
--- resources/views/layouts/partials/footer.blade.php
+++
@@ -1,6 +0,0 @@
-<footer class="footer text-center">
-        <div class="container-fluid clearfix ">
-          <span class="text-muted">Copyright © 2018
-            <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
-        </div>
-</footer>
